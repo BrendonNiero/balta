7816bc8381a896e73bab16538d3419789c5ab7d5
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-05-18 23:21:06 -0300

    learn: data coin

--- coin/LearnCoin/Program.cs
+++ coin/LearnCoin/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+namespace Program
+{
+    public class Program()
+    {
+        public static void Main(string[] args)
+        {
+            decimal salario = 2500.22m; // Quando trabalhamos com moeda
+            Console.WriteLine(salario.ToString(
+            "C",
+            CultureInfo.CreateSpecificCulture("pt-BR"))); // Converte para valor BR
+        }
+    }
+}
