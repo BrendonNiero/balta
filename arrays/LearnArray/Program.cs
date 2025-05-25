1998e013082d88e4f03999f8abffc38beb3e33f9
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-05-24 23:26:26 -0300

    learn: arrays string e int

--- arrays/LearnArray/Program.cs
+++ arrays/LearnArray/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Aprendendo arrays com Andre Baltieri");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> meuArray = new List<int> { 11, 23, 03 };
+        List<string> frutas = new List<string> { "Mexirica", "Caqui", "Kiwi", "Pitaya" };
+
+        Console.WriteLine(string.Join(" | ", meuArray));
+        Console.WriteLine(string.Join(" | ", frutas));
+    }
+}
