de8e2dd93ed2c43da5bcad5a85e3fc82bf1f9f8c
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-05-25 23:07:30 -0300

    learn: Exceptions

---
+++ exceptions/LearnExceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿List<int> valores = new List<int> { 1, 2, 3 };
+
+
+try
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine(valores[i]);
+    }
+} catch(ArgumentOutOfRangeException){
+    Console.WriteLine("Indice não encontrado.");
+}
+
