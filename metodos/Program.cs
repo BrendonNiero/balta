cd53995bbaa33672d8023b61fcfa6577bd35e1e5
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-06-07 22:34:53 -0300

    learn: metodos

---
+++ metodos/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        List<int> Valores = [1, 2, 3, 4, 5, 6];
+        Console.WriteLine(SomarTudo(Valores));
+
+        Console.WriteLine(HoraAtual());
+
+    }
+
+    public static int SomarTudo(List<int> numeros)
+    {
+        return numeros.Sum();
+    }
+
+    public static string HoraAtual()
+    {
+        DateTime hora = DateTime.Now;
+        return hora.ToString("D");
+    }
+}
