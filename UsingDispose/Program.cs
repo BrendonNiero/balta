eb9abf2a642ac4b583d996ebbecbf64b6ed00430
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-06-08 23:54:34 -0300

    learn: dispose

---
+++ UsingDispose/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Recurso : IDisposable
+{
+    public void Usar()
+    {
+        Console.WriteLine("Usando o recurso...");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("Liberando o recurso...");
+    }
+}
