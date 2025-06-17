255a713c948df38cdf834c5334ebb652bacb8ee8
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-06-16 23:35:58 -0300

    learn: funções estáticas

--- classes-estaticas/Program.cs
+++ classes-estaticas/Program.cs
@@ -1,7 +1,23 @@
-﻿public static class ConversorDeTemperatura()
+﻿namespace Program
 {
-    public static double Celcius()
+
+    public class Program
+    { 
+        public static void Main()
+        {
+            var resultadoSoma = Calcular.Somar(1, 5);
+            Console.WriteLine(resultadoSoma);
+        }
+    }
+    public static class Calcular
     {
-        return double;
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
