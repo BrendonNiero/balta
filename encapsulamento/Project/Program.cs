673a4d5e087525c71361458e2069310696683b92
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-07-02 23:50:09 -0300

    init: encapsulamento

---
+++ encapsulamento/Project/Program.cs
@@ -0,0 +1,20 @@
+﻿public class ContaBancaria
+{
+    private decimal saldo;
+
+    public void Depositar(decimal valor)
+    {
+        if (valor > 0)
+        {
+            saldo += valor;
+        }
+    }
+
+    public decimal ConsultarSaldo()
+    {
+        return saldo;
+    }
+}
+
+// saldo está encapsulado
+// -> não pode ser acessado diretamente de fora.
---
+++ encapsulamento/Project/readme.md
@@ -0,0 +1,3 @@
+# O que é Encapsulamento?
+
+Encapsulamento é o conceito do esconder os detalhes internos de uma classe e expor apenas o necessário para o uso externo.
